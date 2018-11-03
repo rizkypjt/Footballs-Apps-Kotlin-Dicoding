0cb2300b33a8cf4a7b22da5269deff00bbf0f96e
rizkypjt <rizkyhidayatpanjaitan97@gmail.com>
2018-11-03 13:56:09 +0700

    Create Acivity Main & RecyclerView Adapter

---
+++ app/src/main/java/panjaitan/rizky/app/football/Item.kt
@@ -0,0 +1,8 @@
+package panjaitan.rizky.app.football
+
+import android.media.Image
+
+/**
+ * Created by admin on 30/10/2018.
+ */
+data class Item (val name: String?, val image: Int?)
--- app/src/main/java/panjaitan/rizky/app/football/MainActivity.kt
+++ app/src/main/java/panjaitan/rizky/app/football/MainActivity.kt
@@ -2,11 +2,34 @@ package panjaitan.rizky.app.football
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutCompat
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import panjaitan.rizky.app.football.adapter.RecyclerViewAdapter
 
-class MainActivity : AppCompatActivity() {
+private var items: MutableList<Item> = mutableListOf()
+
+class MainActivity : AppCompatActivity()  {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val list = findViewById<RecyclerView>(R.id.club_list)
+        initData()
+
+        list.layoutManager = LinearLayoutManager(this)
+        list.adapter = RecyclerViewAdapter(this, items)
     }
+
+    private fun initData(){
+        val name = resources.getStringArray(R.array.club_name)
+        val image = resources.obtainTypedArray(R.array.club_image)
+        items.clear()
+        for (i in name.indices){
+            items.add(Item(name[i],
+                    image.getResourceId(i, 0)))
+        }
+        image.recycle()
+    }
+
 }
